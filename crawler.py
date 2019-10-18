7cbce6ba1d797e0cccb22c30604778bfa4c82084
love-you-3000 <405899016@qq.com>
2019-10-18 11:44:17 +0800

    first commit

---
+++ crawler.py
@@ -0,0 +1,42 @@
+import requests
+from urllib.parse import urlencode
+import  json
+import re
+from tqdm import *
+import time
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
+}
+bash_url = 'https://video.coral.qq.com/filmreviewr/c/upcomment/q0t4yvm2i52eoub?'
+
+
+f= open('result.txt','w')
+page = 20
+print('共爬取'+str(page)+'页：')
+commentid = '6051606569879688737'
+for i in tqdm(range(page)):
+    try:
+        parse = {
+            'commentid': commentid,
+            'reqnum': '3',
+            'callback': 'jQuery112406841701832215665_1571365605027'
+        }
+        url = bash_url+urlencode(parse)
+        pat = 'jQuery.*?\((.*)\)'
+        data =requests.get(url,headers=headers).text
+        json_dict =re.findall(pat,data)[0]
+        texts = json.loads(json_dict)
+
+        commentid = (texts.get('data')).get('last')
+        for contents in (texts.get('data')).get('commentid'):
+            title  = contents.get('title')
+            abstract = contents.get('abstract')
+            f.write(title+'\n')
+            f.write(abstract+'\n')
+            f.write('='*20+'\n')
+    except Exception as err:
+        print(err)
+
+f.close()
+time.sleep(1)
+print('写入文件完成。')
